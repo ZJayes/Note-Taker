4026ae7f31e37dbfedaae117535923d2f112359c
Zachary Jayes <zacjayes@gmail.com>
2021-01-31 18:50:21 -0600

    finished get api/notes page

--- Develop/server.js
+++ Develop/server.js
@@ -19,12 +19,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
+// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
 
-
-
-// app.get('/api/notes', (req, res) => {
-//     res.sendFile
-// })
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname + '/db/db.json'))
+})
 
 
 
